9e1874ba7632680ec9dd9376baf69c8a41230cab
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-12-12 10:34:49 -0300

    fear: rota put/customers/:id atualizando dados de usuário

--- controllers/customers.controller.js
+++ controllers/customers.controller.js
@@ -62,3 +62,34 @@ export async function setCustomers(req, res) {
     }
 
 }
+
+export async function updateCustomers(req, res){
+    const {id} = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    try {
+        const customerData = await connection.query('SELECT * FROM customers WHERE "id" = ($1);', [id]);
+        
+        if (customerData.rows.length === 0) {
+            return res.status(404).send("Customer not Found!");
+        }
+        
+        const customer = await connection.query('SELECT * FROM customers WHERE "cpf" = ($1);', [cpf]);
+        
+        if (customer.rows.length > 0) {
+            return res.status(409).send("Cpf already exists");
+        }
+
+        else {
+            await connection.query(`
+            UPDATE customers 
+            SET name=($1), phone=($2), cpf=($3), birthday=($4)
+            WHERE id = ($5)`, [name, phone, cpf, birthday, id]);
+            return res.sendStatus(201);
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
--- routes/customers.route.js
+++ routes/customers.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getCustomers, getCustomersById, setCustomers } from "../controllers/customers.controller.js";
+import { getCustomers, getCustomersById, setCustomers, updateCustomers } from "../controllers/customers.controller.js";
 import { customerValidation } from "../middlewares/customers.middleware.js";
 
 const customerRouter = Router();
@@ -7,5 +7,6 @@ const customerRouter = Router();
 customerRouter.get("/customers", getCustomers);
 customerRouter.get("/customers/:id", getCustomersById);
 customerRouter.post("/customers", customerValidation, setCustomers);
+customerRouter.put("/customers/:id", customerValidation,updateCustomers);
 
 export default customerRouter;
